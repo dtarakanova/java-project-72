f7b62b4c238764ea13ddbbb23cfc9c7b65e07fac
dtarakanova <dshargalina@gmail.com>
2024-01-29 10:52:45 +0300

    Javalin add SLF4J add App.java add

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,14 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("io.javalin:javalin:5.6.2")
+    implementation("io.javalin:javalin-bundle:5.6.2")
+    implementation("io.javalin:javalin-rendering:5.6.2")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
+}
+
+application {
+    mainClass.set("hexlet.code.App")
 }
 
 tasks.test {
---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,16 @@
+package hexlet.code;
+import io.javalin.Javalin;
+
+public class App {
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> config.plugins.enableDevLogging());
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
