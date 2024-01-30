9565fb57f546b54d426e4ea5be038df76aa1f2a0
dtarakanova <dshargalina@gmail.com>
2024-01-30 09:56:01 +0300

    Create app with Hello World message

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,10 +15,10 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
     implementation("io.javalin:javalin:5.6.2")
     implementation("io.javalin:javalin-bundle:5.6.2")
     implementation("io.javalin:javalin-rendering:5.6.2")
-    implementation("org.slf4j:slf4j-simple:2.0.9")
 }
 
 application {
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,16 +1,26 @@
 package hexlet.code;
 import io.javalin.Javalin;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 public class App {
-    public static Javalin getApp() {
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.valueOf(port);
+    }
+
+    public static Javalin getApp() throws IOException, SQLException {
 
         var app = Javalin.create(config -> config.plugins.enableDevLogging());
+        app.get("/", ctx -> ctx.result("Hello World"));
 
         return app;
     }
 
-    public static void main(String[] args) {
-        Javalin app = getApp();
-        app.start(7070);
+    public static void main(String[] args) throws IOException, SQLException {
+        var app = getApp();
+        app.start(getPort());
     }
 }
